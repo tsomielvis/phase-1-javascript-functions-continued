34fb42c015a2c8a938aad266066379f5727a352d
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-07-10 20:46:13 +0300

    latest

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+   }
+   function mondayWork(whereToGo = "go to the office"){
+    return `This Monday, I will ${whereToGo}.`;
+}
+
+function wrapAdjective(symbol = "'*'") {
+    return function (word = "special"){
+        return`You are ${symbol}${word}${symbol}!`
+    }
+}
